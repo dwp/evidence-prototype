6ccf03f2395a193224a0411659d58bb185baa74a
Leah Smith <leah.smith3@engineering.digital.dwp.gov.uk>
2026-01-21 12:15:36 +0000

    homepage routing
    GitOrigin-RevId: f5002e7176686022d4393ab5f1a434a39103845d

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,14 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+//Homepage routing
+router.post("/homepage", (req, res) =>{
+    if(req.body.yourBenefits === "radio") {
+        res.redirect("/homepage/radio");
+    } else {
+        // could also do === "navigation" but don't really care
+        res.redirect("/homepage/navigation"); 
+    }
+})
