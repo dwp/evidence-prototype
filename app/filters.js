9802f7390ad010e810ce63f3682fe58c9a5e39a4
Alexandra Aggelidis <alexandra.aggelidis@engineering.digital.dwp.gov.uk>
2026-06-04 14:04:22 +0100

    actions-updates2-upload-changes
    - changes to upload screens
    - date filter added to prototype
    
    GitOrigin-RevId: 925fdc643fb3c1de3ce4134d8d80ee35aa7c6059

--- app/filters.js
+++ app/filters.js
@@ -6,5 +6,26 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
-// Add your filters here
 
+// Format date filter that turns day, month and year into a GOV.UK style format
+
+govukPrototypeKit.views.addFunction('formatDate', function (day, month, year) {
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    return `${parseInt(day, 10)} ${months[parseInt(month, 10) -1]} ${year}`
+})
+
+// Add your filters here
